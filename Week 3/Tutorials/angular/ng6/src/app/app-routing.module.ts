9c46f201f023336145e70b164ace250f8351c8e5
Evan Huang <reasonstouninstall+github2@protonmail.com>
2018-08-17 00:02:54 +0800

    Finishing up Tutorial

--- Week 3/Tutorials/angular/ng6/src/app/app-routing.module.ts
+++ Week 3/Tutorials/angular/ng6/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+//importing components we wish to route
+import { UsersComponent } from './users/users.component';
+import { DetailsComponent } from './details/details.component';
+import { PostsComponent } from './posts/posts.component';
+
+// Define routes
+const routes: Routes = [
+    {
+        path: '',
+        component: UsersComponent
+    },
+    {
+        path: 'details/:id',
+        component: DetailsComponent
+    },
+    {
+        path: 'posts',
+        component: PostsComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
